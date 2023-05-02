2cb8a2f4e0735e96348f3b87cb17c5e29e725c5a
Anton <tosha.hd2@gmail.com>
2023-05-02 15:24:26 +0300

    task2 update

--- src/task2.java
+++ src/task2.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class task2 {
+    /**
+     * Если необходимо, исправьте данный код
+     * (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+     */
     public static void main(String[] args) {
         try {
             int[] intArray = {2,4,6,8,10,12,14,16,18,20};
