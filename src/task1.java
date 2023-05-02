ca8c506b5db9cef04f0d4c68ffb4ec86c12a2d2d
Anton <tosha.hd2@gmail.com>
2023-05-02 15:07:08 +0300

    task1

--- src/task1.java
+++ src/task1.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class task1 {
+import java.util.Scanner;
+
+public class task1 {
+    /**
+     Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
+     и возвращает введенное значение.
+     Ввод текста вместо числа не должно приводить к падению приложения,
+     вместо этого, необходимо повторно запросить у пользователя ввод данных.
+     */
+    public static void main(String[] args) {
+        boolean correct = false;
+
+        while (!correct){
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Введите дробное число(разделитель - зяпятая):");
+                float number = sc.nextFloat();
+                correct = true;
+                System.out.println("Вы ввели число " + number);
+            } catch (RuntimeException e){
+                System.out.println("Некорректный ввод, повторите попытку.");
+            }
+        }
+
+    }
 }
